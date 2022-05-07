a145cfea28c62276b59b81dbf71833536da9e625
me-cafebabe <me_cafebabe@hotmail.com>
2022-05-08 05:20:19 +0800

    Mi8937: overlay: Decommonize config_deviceHardwareKeys

--- device.mk
+++ device.mk
@@ -14,7 +14,8 @@ TARGET_SCREEN_WIDTH := 720
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
     xiaomi_ugg_overlay \
